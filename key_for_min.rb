9da24d56c0ff32e0c64ca0ffcea08f99e297f09d
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 01:29:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,6 @@ def key_for_min_value(name_hash)
   smallest_name = nil 
   smallest_num = nil 
   name_has. each do |name, num|
-    if smallest_num == nil ||
+    if smallest_num == nil || num <= sma
 
 end
