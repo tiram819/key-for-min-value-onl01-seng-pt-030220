df41df4db03b2991bb56ee9a633c251e50aff41c
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 01:25:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_hash = nil
+  smallest_name = nil 
+  smallest_num = nil 
+  
 
 end
